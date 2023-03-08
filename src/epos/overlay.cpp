98408d34c1bc19c54a8003a2291fef96c4e42623
Alexej Harm <alexej.h@xiphos.de>
2023-03-08 13:00:22 +0100

    improved render performance

--- src/epos/overlay.cpp
+++ src/epos/overlay.cpp
@@ -27,29 +27,44 @@ overlay::overlay(HINSTANCE instance, HWND hwnd, long cx, long cy)
   HR(d3d_device.As(&dxgi_device));
 
   ComPtr<IDXGIFactory2> dxgi_factory;
-  HR(CreateDXGIFactory2(DXGI_CREATE_FACTORY_DEBUG, __uuidof(dxgi_factory), &dxgi_factory));
+#ifdef NDEBUG
+  constexpr UINT dxgi_factory_flags = 0;
+#else
+  constexpr UINT dxgi_factory_flags = DXGI_CREATE_FACTORY_DEBUG;
+#endif
+  HR(CreateDXGIFactory2(dxgi_factory_flags, __uuidof(dxgi_factory), &dxgi_factory));
 
   DXGI_SWAP_CHAIN_DESC1 description = {};
   description.Width = static_cast<decltype(description.Width)>(cx);
   description.Height = static_cast<decltype(description.Height)>(cy);
   description.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
   description.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-  description.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
-  description.BufferCount = 2;
+  description.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
   description.SampleDesc.Count = 1;
+  description.BufferCount = 2;
   description.AlphaMode = DXGI_ALPHA_MODE_PREMULTIPLIED;
+  description.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_TEARING;
   HR(dxgi_factory->CreateSwapChainForComposition(dxgi_device.Get(), &description, nullptr, &sc_));
 
   // Create device context.
   ComPtr<ID2D1Factory2> d2d_factory;
-  HR(D2D1CreateFactory(
-    D2D1_FACTORY_TYPE_SINGLE_THREADED,
-    { D2D1_DEBUG_LEVEL_INFORMATION },
-    d2d_factory.GetAddressOf()));
+  constexpr auto d2d_factory_type = D2D1_FACTORY_TYPE_SINGLE_THREADED;
+#ifdef NDEBUG
+  const D2D1_FACTORY_OPTIONS d2d_factory_options{ D2D1_DEBUG_LEVEL_NONE };
+#else
+  const D2D1_FACTORY_OPTIONS d2d_factory_options{ D2D1_DEBUG_LEVEL_INFORMATION };
+#endif
+  HR(D2D1CreateFactory(d2d_factory_type, d2d_factory_options, d2d_factory.GetAddressOf()));
 
   ComPtr<ID2D1Device1> d2d_device;
   HR(d2d_factory->CreateDevice(dxgi_device.Get(), d2d_device.GetAddressOf()));
-  HR(d2d_device->CreateDeviceContext(D2D1_DEVICE_CONTEXT_OPTIONS_NONE, &dc_));
+
+#ifdef NDEBUG
+  constexpr auto d2d_device_options = D2D1_DEVICE_CONTEXT_OPTIONS_ENABLE_MULTITHREADED_OPTIMIZATIONS;
+#else
+  constexpr auto d2d_device_options = D2D1_DEVICE_CONTEXT_OPTIONS_NONE;
+#endif
+  HR(d2d_device->CreateDeviceContext(d2d_device_options, &dc_));
 
   // Create bitmap render target for swap chain.
   ComPtr<IDXGISurface2> surface;
@@ -108,7 +123,7 @@ void overlay::run() noexcept
       render();
       const auto result = dc_->EndDraw();
       assert(SUCCEEDED(result));
-      sc_->Present(1, DXGI_PRESENT_RESTART);
+      sc_->Present(0, DXGI_PRESENT_RESTART);
       cmd = command_.exchange(command::none);
     }
   }
